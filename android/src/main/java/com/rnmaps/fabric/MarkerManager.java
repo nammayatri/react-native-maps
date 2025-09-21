191b15de634920a01de4cb6fb1e731bdf0fc4d4b
Salah Ghanim <salah.ghanim@gmail.com>
2025-09-21 20:26:25 +0200

    fix: android marker update NPE (#5744)
    fix(android): handle when Marker is null

--- android/src/main/java/com/rnmaps/fabric/MarkerManager.java
+++ android/src/main/java/com/rnmaps/fabric/MarkerManager.java
@@ -274,7 +274,7 @@ public class MarkerManager extends ViewGroupManager<MapMarker> implements RNMaps
     public void redraw(MapMarker view) {
         view.redraw();
     }
-    
+
     @Override
     public void addView(MapMarker parent, View child, int index) {
         // if an <Callout /> component is a child, then it is a callout view, NOT part of the
@@ -290,7 +290,9 @@ public class MarkerManager extends ViewGroupManager<MapMarker> implements RNMaps
                         int newWidth = right - left;
                         int newHeight = bottom - top;
                         MapMarker marker = (MapMarker) v.getParent();
-                        marker.update(newWidth, newHeight);
+                        if(marker != null){
+                            marker.update(newWidth, newHeight);
+                        }
                     }
                 });
             }
